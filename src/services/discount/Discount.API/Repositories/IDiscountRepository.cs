7d0ff2d363fa064fd627a780ce7a1968f0259cff
Jonathan Araque <jonathan.araque@bizagi.com>
2022-02-14 18:34:38 -0500

    Create Discount.API project with PostgreSQL discount.db

---
+++ src/services/discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.API.Entities;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetByProductName(string productName);
+        Task<bool> Create(Coupon coupon);
+        Task<bool> Delete(string productName);
+        Task<bool> Update(Coupon coupon);
+    }
+}
