6036090167bfa9e2909cc556b5a410c738a4744a
Jonathan Araque <jaap5227@gmail.com>
2022-03-29 15:55:03 -0500

    Added Status.WebApp to monitor all health

---
+++ src/web-apps/Status.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using HealthChecks.UI.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Status.WebApp.Models;
+
+namespace Status.WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly Options _options;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        _options = new Options();
+    }
+
+    public IActionResult Index()
+    {
+        return Redirect(_options.UIPath);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ src/web-apps/Status.WebApp/Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddHealthChecksUI()
+    .AddInMemoryStorage();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapHealthChecksUI();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
